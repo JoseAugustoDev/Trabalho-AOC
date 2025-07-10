f40786c34a1bc7b20fc49873a65e54fb863965a4
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2025-07-10 14:30:27 -0300

    Adicionando comentarios

--- Problema1.asm
+++ Problema1.asm
@@ -1,7 +1,6 @@
 org 100h
 
 input:
-    ; Mostrando mensagem no terminal
     mov dx, offset msg
     mov ah, 09h
     int 21h     
@@ -21,8 +20,7 @@ input:
     sub al, '0'
     mov bh, al    
 
-    ; Calcula a idade da seguinte maneira => (dezena * 10) + unidade, sendo dezena o primeiro digito e unidade o segundo
-    ; Formando o numero digitado
+    ; idade = (dezena * 10) + unidade
     mov al, bl
     mov ah, 0
     mov cx, 10
@@ -31,25 +29,30 @@ input:
 
     mov [idade], al
 
-    ; Verifica se o numero digitado foi negativo
+    ; Se for menor que zero é invalido
     cmp al, 0
     jl invalido
 
 validacao:
-    mov al, [idade] ; Idade no registrador al
+    mov al, [idade] ; al recebe o valor da idade
+    
+    ; compara com a primeira marca (12)
     mov bl, primeiraMarca
     cmp al, bl 
     
     jnge crianca
 
+    ; compara com a segunda marca (18)
     mov bl, segundaMarca
     cmp al, bl
     jnge adolescente
 
+    ; compara com a segunda marca (60)
     mov bl, terceiraMarca
     cmp al, bl
     jnge adulto
 
+    ; Se nao for nenhuma das outras (else)
     jmp idoso
 
 crianca:    
@@ -76,8 +79,11 @@ imprimir:
     mov ah, 0Eh
     mov al, 13
     int 10h
+
     mov al, 10
     int 10h
+
+    ; Imprime o valor em dx (mensagem)
     mov ah, 09h
     int 21h
 
@@ -89,7 +95,7 @@ primeiraMarca   db 12
 segundaMarca    db 18
 terceiraMarca   db 60
 
-msg             db "Digite sua idade (2 digitos, Ex.: 9 anos ->  Digite 09 ): $"
+msg             db "Digite sua idade (2 digitos): $"
 se_crianca      db "Com essa idade eh: crianca $"
 se_adolescente  db "Com essa idade eh: adolescente $"
 se_adulto       db "Com essa idade eh: adulto $"
